dbd63a5fbe55d2328f04738b8ddfb200486bcc2d
b-s-x <b.s.x.mars@gmail.com>
2020-09-10 21:02:37 +0300

    add two and three page component render

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -1,15 +1,20 @@
 <template lang='pug'>
 	div(class='page')
 		part-one/
+		part-two/
+		part-three/
 </template>
 
 <script>
 import PartOne from '@/components/render/PagePartOne.vue'
-
+import PartTwo from '@/components/render/PagePartTwo.vue'
+import PartThree from '@/components/render/PagePartThree.vue'
 
 export default {
 	components: {
 		PartOne,
+		PartTwo,
+		PartThree
 	}
 }
 </script>
